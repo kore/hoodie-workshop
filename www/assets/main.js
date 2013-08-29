f405c32ee22551988adbe95e17fb0f183f6af929
Kore Nordmann <mail@kore-nordmann.de>
2013-08-29 15:39:39 +0200

    Apply client side map-reduce on payments data

--- www/assets/main.js
+++ www/assets/main.js
@@ -1,17 +1,67 @@
-// initialize Hoodie
+// Initialize Hoodie
 var hoodie  = new Hoodie()
 
-// initial load of all payment items from the store
-hoodie.store.findAll('payment').then( function(payments) {
-  payments.sort( sortByCreatedAt ).forEach( addPayment )
-})
+// Initial load of all payment items from the store
+function refreshViews() {
+  hoodie.store.findAll('payment').then( function(payments) {
+    // Refresh Users view
+    renderAggregateView(
+      _.map(
+        _.groupBy(
+          payments,
+          function (payment) {
+            return payment.user;
+          }
+        ),
+        sumUpPayments
+      ),
+      "#userPayments"
+    );
 
-// when a new payment gets stored, add it to the UI
-hoodie.store.on('add:payment', addPayment)
-// clear payment list when the get wiped from store
-hoodie.account.on('signout', clearPayments)
+    // Refresh Events view
+    renderAggregateView(
+      _.map(
+        _.groupBy(
+          payments,
+          function (payment) {
+            return payment.event;
+          }
+        ),
+        sumUpPayments
+      ),
+      "#eventPayments"
+    );
 
-// handle creating a new task
+    // Refresh most recent paied view
+    renderPaymentsView(
+      _.sortBy(
+        _.filter(
+          payments,
+          function (payment) {
+            return payment.state === "paied";
+          }
+        ),
+        function (a, b) {
+          return a.createdAt - b.createdAt;
+        }
+      ),
+      "#recentlyPaied"
+    );
+  });
+}
+refreshViews();
+
+// When a new payment gets stored, add it to the UI
+hoodie.store.on('add:payment', refreshViews)
+// Clear payment list when the get wiped from store
+hoodie.account.on(
+  'signout',
+  function() {
+    $('#paymentTable tbody').html('');
+  }
+);
+
+// Handle creating a new payment
 $('#paymentForm').on('submit', function(event) {
   hoodie.store.add(
     'payment', {
@@ -23,21 +73,63 @@ $('#paymentForm').on('submit', function(event) {
 
   $(event.target).find("input").val("");
 
-  event.preventDefault(false);
+  event.preventDefault();
   return false;
-})
+});
 
-function addPayment( payment ) { 
-  $('#paymentTable tbody').append('<tr>' +
-    '<td>' + payment.user + '</td>' +
-    '<td>' + payment.event + '</td>' +
-    '<td>' + payment.value.toFixed(2) + '</td>' +
-    '<td class="' + payment.state + '">' + payment.state + '</td>' +
-  '</tr>');
+// Bootstrap code to activate tabs in view
+$('#paymentTabs a').click(function (event) {
+  $(this).tab('show');
+  event.preventDefault();
+  return false
+});
+
+function renderAggregateView(aggregatePayments, table) {
+  $(table + ' tbody').html("");
+  _.map(
+    aggregatePayments,
+    function(aggregatePayment) {
+      $(table + ' tbody').append('<tr>' +
+        '<td>' + aggregatePayment.name + '</td>' +
+        '<td>' + aggregatePayment.open.toFixed(2) + '€</td>' +
+        '<td>' + aggregatePayment.paied.toFixed(2) + '€</td>' +
+      '</tr>');
+    }
+  );
 }
-function clearPayments() {
-  $('#paymentTable tbody').html('');
+
+function renderPaymentsView(payments, table) {
+  $(table + ' tbody').html("");
+  _.map(
+    payments,
+    function(payment) {
+      $(table + ' tbody').append('<tr>' +
+        '<td>' + payment.user + '</td>' +
+        '<td>' + payment.event + '</td>' +
+        '<td>' + payment.value.toFixed(2) + '€</td>' +
+        '<td class="' + payment.state + '">' + payment.state + '</td>' +
+      '</tr>');
+    }
+  );
 }
-function sortByCreatedAt(a, b) { 
-  return a.createdAt > b.createdAt
+
+function sumUpPayments(payments, context) {
+  return _.reduce(
+    _.flatten(
+      _.toArray(
+        _.groupBy(
+          payments,
+            function (payment) {
+            return payment.state;
+          }
+        )
+      )
+    ),
+    function (result, payment) {
+      result[payment.state] += payment.value;
+      return result;
+    },
+    {name: context, open: 0, paied: 0}
+  );
 }
+
